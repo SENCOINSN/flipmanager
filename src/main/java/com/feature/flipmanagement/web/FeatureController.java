86a3126b5f53177789d6ebb87a0ebab25b0ca282
sencoinsn <seyeadama03@outlook.com>
2024-09-22 22:40:41 +0200

    template content and display features

--- src/main/java/com/feature/flipmanagement/web/FeatureController.java
+++ src/main/java/com/feature/flipmanagement/web/FeatureController.java
@@ -1,11 +1,17 @@
 package com.feature.flipmanagement.web;
 
 import com.feature.flipmanagement.constants.ApiPath;
+import com.feature.flipmanagement.dto.FeatureRequest;
 import com.feature.flipmanagement.service.IFeatureFlip;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -15,9 +21,21 @@ public class FeatureController {
 
     private final IFeatureFlip featureService;
 
-  @GetMapping("/console")
-    public String home() {
+    @GetMapping("/console")
+    public String home(Model model) {
+        model.addAttribute("features", featureService.listAllFeatures());
         return "feature";
+    }
+
+    @PostMapping("/create")
+    public String createFeature(@Valid FeatureRequest request, BindingResult result,
+                                RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            return "feature";
+        }
+        featureService.createFeature(request);
+        redirectAttributes.addFlashAttribute("message", "Feature created successfully!");
+        return "redirect:/feature/console";
     }
 
 }
---
+++ src/main/resources/static/js/datatable-demo.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('#dataTable').DataTable();
+    $('#dataTable1').DataTable();
+    $('#dataTable2').DataTable();
+    $('#dataTable3').DataTable();
+    $('#dataTable4').DataTable();
+    $('#dataTable5').DataTable();
+    $('#dataTable6').DataTable();
+    $('#dataTable7').DataTable();
+    $('#dataTable8').DataTable();
+    $('#dataTable9').DataTable();
+    $('#dataTable10').DataTable();
+    $('#dataTable11').DataTable();
+
+});
